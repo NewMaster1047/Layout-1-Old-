1a90c0de88accaab5acb855fd46cd337045fb125
NewMaster1047 <jaloliddinmine00@gmail.com>
2023-11-28 16:16:18 +0500

    Test

---
+++ js/main.js
@@ -0,0 +1,221 @@
+$(window).scroll(function() {
+    $('.section_2_title').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 650) {
+            $(this).addClass("fadeInDown")
+        }
+    })
+    
+})
+$(window).scroll(function() {
+    $('.section_2_title_text').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 650) {
+            $(this).addClass("fadeInUp")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.content_3').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 650) {
+            $(this).addClass("fadeInLeft")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.content_2').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 650) {
+            $(this).addClass("fadeInLeft")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.content_1').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 650) {
+            $(this).addClass("fadeInLeft")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.anim_btn').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 800) {
+            $(this).addClass("fadeInUp")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.section_3_title').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 800) {
+            $(this).addClass("fadeInLeft")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.section_3_img').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 800) {
+            $(this).addClass("fadeInRight")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.apps').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 800) {
+            $(this).addClass("fadeInUp")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.section_4_title').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 800) {
+            $(this).addClass("fadeInDown")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.section_4_content').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 800) {
+            $(this).addClass("fadeInUp")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.section_5_title').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 800) {
+            $(this).addClass("fadeInDown")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.section_5_title_text').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 800) {
+            $(this).addClass("fadeInLeft")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.section_5_img').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 800) {
+            $(this).addClass("fadeInRight")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.section_5_btn').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 800) {
+            $(this).addClass("fadeInUp")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.section_6_butt').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 800) {
+            $(this).addClass("fadeIn")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.post_title').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 800) {
+            $(this).addClass("fadeInDown")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.post_text').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 800) {
+            $(this).addClass("fadeInRight")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.post_post').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 800) {
+            $(this).addClass("fadeInLeft")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.section_8_content_1').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 800) {
+            $(this).addClass("fadeInLeft")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.section_8_content_2').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 800) {
+            $(this).addClass("fadeInRight")
+        }
+    })
+})
+$(window).scroll(function() {
+    $('.footer_content').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 900) {
+            $(this).addClass("bounceIn")
+        }
+    })
+})
